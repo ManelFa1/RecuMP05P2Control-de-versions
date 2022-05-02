e2ab41a66dd224c9eabb5e6cd98216b819cee381
Manel <manelfa@elpuig.xeill.net>
2022-05-02 16:05:09 +0200

    1

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola copiaº1");
+        System.out.println("Hola copiaº1");
     }
 }
